9691c6a693dd407d871966d503e52e8c569af44d
Zeuxis Lo <seekstudio@gmail.com>
2015-01-15 23:44:09 +0800

    add new design in home/index.html page

--- public/static/client/js/default.js
+++ public/static/client/js/default.js
@@ -1,7 +1,55 @@
+var renderTemplate = function(templateSelector, params) {
+    return Handlebars.compile($(templateSelector).html())(params);
+}
+
 ;(function($) {
 
     $(function() {
         $('.ui.checkbox').checkbox();
+
+        $(".drop-area").dmUploader({
+            url              : '/upload',
+            dataType         : 'json',
+            allowedTypes     : 'image/*',
+            onNewFile        : function(id, file) {
+                if (id === 0) {
+                    $(".file-area > .upload-first").hide();
+                    $(".file-area > .files").show();
+                }
+
+                $(".file-area > .files").append(renderTemplate("#preview-file", {
+                    "id"  : id,
+                    "file": file
+                }));
+
+                if (typeof FileReader !== "undefined") {
+                    var reader = new FileReader();
+                    var img = $("#preview-file-" + id).find("img").eq(0);
+
+                    reader.onload = function (e) {
+                        img.attr('src', e.target.result);
+                    }
+
+                    reader.readAsDataURL(file);
+                }else{
+                    $('#demo-files').find('.demo-image-preview').remove();
+                }
+            },
+            onUploadProgress : function() {
+
+            },
+            onUploadSuccess  : function(id, data) {
+
+            },
+        });
+
+        $(".drop-area")
+            .on("dragover", function(e) {
+                $(this).addClass('warning message');
+            })
+            .on('dragleave', function(e) {
+                $(this).removeClass('warning message');
+            });
     });
 
 })(jQuery);
